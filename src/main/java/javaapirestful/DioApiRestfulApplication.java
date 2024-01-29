67f95aea8b450020986aa395e2d1535c6398da03
NaiaraSuelen <na_suelen25@hotmail.com>
2024-01-29 18:07:58 -0300

    First commit

---
+++ src/main/java/javaapirestful/DioApiRestfulApplication.java
@@ -0,0 +1,13 @@
+package javaapirestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioApiRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioApiRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/javaapirestful/DioApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package javaapirestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioApiRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
